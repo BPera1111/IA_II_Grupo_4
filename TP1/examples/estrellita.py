38ff0c0a28289a1b53c537c16ca2bfa3d779cb93
BPera1111 <bruno.peralta@ms.uncu.edu.ar>
2024-03-27 16:11:17 -0300

    Revert "Estrella OO + Recorrido"
    This reverts commit 1afc8dc16eccb0dc375d2b014d4bfc5eab89def4.

--- TP1/examples/estrellita.py
+++ TP1/examples/estrellita.py
@@ -6,28 +6,39 @@
     # si es el objetifo: fin
     # sinó es el objetivo: el nodo seleccionado pasa a ser el nodo actual y se repite desde el punto (3)
 
+class tablero:
+
+    def __init__(self,filas,columnas):
+        self.filas = filas
+        self.columnas = columnas
+        self.matriz = None
+        self.crear_tablero()
 
 
+    def crear_tablero(self): #retorna la matriz (tablero)
+        # Definir las dimensiones de la matriz
+        # filas = 21  # Puedes cambiar este valor según tus necesidades
+        # columnas = 19  # Puedes cambiar este valor según tus necesidades
 
-def tablero(): #retorna la matriz (tablero)
-    # Definir las dimensiones de la matriz
-    filas = 21  # Puedes cambiar este valor según tus necesidades
-    columnas = 19  # Puedes cambiar este valor según tus necesidades
+        # Inicializar una lista vacía para contener la matriz
+        self.matriz = []
 
-    # Inicializar una lista vacía para contener la matriz
-    matriz = []
+        # Generar la matriz con las características requeridas
+        for i in range(self.filas):
+            fila = []
+            for j in range(self.columnas):
+                # Verificar si la fila o la columna son múltiplos de 4 (para filas y columnas basadas en 0)
+                if i % 5 == 0 or i == 0 or j == 0 or j % 3 == 0:
+                    fila.append(0)
+                else:
+                    fila.append(1)
+            self.matriz.append(fila)
+        
+    def actualiza_tablero(self, x, y, valor):
+        self.matriz[x][y] = valor
+        draw.actualiza_pantalla(self.matriz)
 
-    # Generar la matriz con las características requeridas
-    for i in range(filas):
-        fila = []
-        for j in range(columnas):
-            # Verificar si la fila o la columna son múltiplos de 4 (para filas y columnas basadas en 0)
-            if i % 5 == 0 or i == 0 or j == 0 or j % 3 == 0:
-                fila.append(0)
-            else:
-                fila.append(1)
-        matriz.append(fila)
-    return matriz
+
 
 class Nodo:
     def __init__(self, x, y, h, g, f):
@@ -41,16 +52,15 @@ class Nodo:
 
      
     def encuentra_vecinos(self, matriz, objetivo,actual):
-        filas = len(matriz)
-        columnas = len(matriz[0])
-        lista_vecinos = []
+        filas = len(matriz.matriz)
+        columnas = len(matriz.matriz[0])
+        lista_vecinos = [] #mostro a todas las matriz hay que ponerles el .matriz para que entren al atributo del objeto
 
         # Verificar vecino superior
-        if actual.x > 0 and matriz[actual.x - 1][actual.y] == 0:
+        if actual.x > 0 and matriz.matriz[actual.x - 1][actual.y] == 0:
             h = abs(objetivo.x - (actual.x - 1)) + abs(objetivo.y - (actual.y))
             f=h+actual.g+1
-            vecino1 = Nodo(actual.x - 1, actual.y, h, actual.g,f)
-            vecino1.padre = actual
+            vecino1 = Nodo(actual.x - 1, actual.y, h, actual.g,f,)
             lista_vecinos.append(vecino1)
 
         # Verificar vecino inferior
@@ -58,7 +68,6 @@ class Nodo:
             h = abs(objetivo.x - (actual.x + 1)) + abs(objetivo.y - actual.y)
             f=h+actual.g+1
             vecino2 = Nodo(actual.x + 1, actual.y, h, actual.g,f)
-            vecino2.padre = actual
             lista_vecinos.append(vecino2)
 
         # Verificar vecino izquierdo
@@ -66,7 +75,6 @@ class Nodo:
             h = abs(objetivo.x - (actual.x)) + abs(objetivo.y - (actual.y - 1))
             f=h+actual.g+1
             vecino3 = Nodo(actual.x, actual.y - 1, h, actual.g,f)
-            vecino3.padre = actual
             lista_vecinos.append(vecino3)
 
         # Verificar vecino derecho
@@ -74,7 +82,6 @@ class Nodo:
             h = abs(objetivo.x - (actual.x)) + abs(objetivo.y - (actual.y + 1))
             f=h+actual.g+1
             vecino4 = Nodo(actual.x, actual.y + 1, h, actual.g,f)
-            vecino4.padre = actual
             lista_vecinos.append(vecino4)
         
         # lista_vecinos.sort(key=lambda x: x.f)
@@ -94,12 +101,6 @@ class ListaAbiertos:
 
     def obtener_nodo_mas_cercano(self):
         return self.nodos[0]
-
-    def contiene(self, nodo):
-        for n in self.nodos:
-            if n.x == nodo.x and n.y == nodo.y: #and n.g < nodo.g:
-                return True
-        return False
     
 class ListaCerrados:
     def __init__(self):
@@ -109,16 +110,13 @@ class ListaCerrados:
         self.nodos.append(nodo)
 
     def contiene(self, nodo):
-        for n in self.nodos:
-            if n.x == nodo.x and n.y == nodo.y: #and n.g < nodo.g:
-                return True
-        return False
+        return nodo in self.nodos
 
 
 class estrella(Nodo):
 
     def __init__(self, mapa, nodo_inicial, nodo_objetivo):
-        self.mapa = mapa
+        self.mapa = mapa.matriz.copy()
         self.nodo_inicial = nodo_inicial
         self.nodo_objetivo = nodo_objetivo
 
@@ -134,70 +132,50 @@ class estrella(Nodo):
             nodo_actual = lista_abiertos.obtener_nodo_mas_cercano()
             lista_abiertos.remover(nodo_actual)
             lista_cerrados.agregar(nodo_actual)
+            print("\nLista abierta: ",lista_abiertos.nodos)
+            print("\nLista cerrada: ",lista_cerrados.nodos)
             # Si el nodo actual es el objetivo, termina el algoritmo
             if nodo_actual.x == nodo_objetivo.x and nodo_actual.y ==nodo_objetivo.y:
-                print("\n\n   anashe")
-                print("\nLlegamos a: "+ str(nodo_actual.x)+","+str(nodo_actual.y))
-                print("\n\nTamaño de la lista abierta: ",len(lista_abiertos.nodos))
-                print("Tamaño de la lista cerrada: ",len(set(lista_cerrados.nodos)))
-                print("Lista abierta:")
-                
-                for i, nodo in enumerate(lista_abiertos.nodos):
-                    try:
-                        print(f"Posición {i+1}: X: {nodo.x}, Y: {nodo.y}, G: {nodo.g}, F: {nodo.f}, H: {nodo.h}, Padre:  {nodo.padre.x},{nodo.padre.y}")
-                    except Exception:
-                        print(f"Posición {i+1}: X: {nodo.x}, Y: {nodo.y}, G: {nodo.g}, F: {nodo.f}, H: {nodo.h}, Padre: None")
-
-                print("Lista cerrada:")
-                for i, nodo in enumerate(lista_cerrados.nodos):
-                    try:
-                        print(f"Posición {i+1}: X: {nodo.x}, Y: {nodo.y}, G: {nodo.g}, F: {nodo.f}, H: {nodo.h}, Padre: {nodo.padre.x},{nodo.padre.y}")
-                    except Exception:
-                        print(f"Posición {i+1}: X: {nodo.x}, Y: {nodo.y}, G: {nodo.g}, F: {nodo.f}, H: {nodo.h}, Padre: None")
-                              
+                print("anashe")
+                print(nodo_actual.x)
+                print(nodo_actual.y)
                 # print("Lista abierta: ",lista_abiertos.nodos)
                 # print("separaion")
                 # print("Lista cerrada: ",lista_cerrados.nodos)
-                return lista_cerrados.nodos
+                break
                 #return nodo_actual.padre
             # Expande el nodo actual
             for nodo_vecino in self.encuentra_vecinos(mapa, nodo_objetivo, nodo_actual):
                 # Calcula la distancia estimada hasta el nodo objetivo
                 costo_estimado = nodo_actual.g + 1 #nodo_vecino.g
-                nodo_vecino.g = costo_estimado
                 # Si el nodo vecino no está en la lista de cerrados
-                #print(lista_abiertos.contiene(nodo_vecino))
-                if not lista_abiertos.contiene(nodo_vecino) and not lista_cerrados.contiene(nodo_vecino):
+                if not lista_cerrados.contiene(nodo_vecino):
                     # Agrega el nodo vecino a la lista de abiertos
                     lista_abiertos.agregar(nodo_vecino)
                     # Actualiza la distancia estimada del nodo vecino
-            
-                       
+                    nodo_vecino.g = costo_estimado
+
+          
 
 # Zona de pruebas
+import draw
                     
 def main():
-    tab = tablero()
+    tab = tablero(21, 19)
+    print(tab.matriz)
+    draw.mostrar_tablero(tab.matriz)
     
-    inicio =[0,0] # input("\nIngrese la posición inicial (x, y): ")
-    objetivo =[5,7] # input("Ingrese la posición objetivo (x, y): ")
-    # inicio = [int(coord) for coord in inicio.split(",")]
-    # objetivo = [int(coord) for coord in objetivo.split(",")]
+    
+    inicio = input("Ingrese la posición inicial (x, y): ")
+    objetivo = input("Ingrese la posición objetivo (x, y): ")
+    inicio = [int(coord) for coord in inicio.split(",")]
+    objetivo = [int(coord) for coord in objetivo.split(",")]
     h = abs(objetivo[0] - inicio[0]) + abs(objetivo[1] - inicio[1])
     nodo_inicial = Nodo(inicio[0], inicio[1], h, 0, h)
     nodo_objetivo = Nodo(objetivo[0], objetivo[1], h ,0, h)
     estre = estrella(tab, nodo_inicial, nodo_objetivo)
-    lista_cerrada=estre.busqueda_a_estrella(tab, nodo_inicial, nodo_objetivo)
-    final=lista_cerrada[-1]
-    recorrido = []
-    x = True
-    while x:
-        if final.padre == None:x = False
+    estre.busqueda_a_estrella(tab, nodo_inicial, nodo_objetivo) 
 
-        print("Posición: ",final.x,final.y)
-        recorrido.append([final.x,final.y])
-        final = final.padre
-    
 if __name__ == "__main__":
     main()              
                     
