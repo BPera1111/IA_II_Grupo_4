9ca14ccfb01a5640da0bafd43c84379f58268916
lauti <lautaropenha@gmail.com>
2024-03-31 22:48:40 -0300

    ej 2 comentarios

--- TP1/examples/ej2/nodo.py
+++ TP1/examples/ej2/nodo.py
@@ -16,7 +16,7 @@ class Nodo:
     def encuentra_vecinos(self, matriz, objetivo,actual):
         filas = len(matriz.matriz)
         columnas = len(matriz.matriz[0])
-        lista_vecinos = [] #mostro a todas las matriz hay que ponerles el .matriz para que entren al atributo del objeto
+        lista_vecinos = [] 
 
         # Verificar vecino superior
         if actual.x > 0 and matriz.matriz[actual.x - 1][actual.y] == 0:
@@ -50,8 +50,6 @@ class Nodo:
             vecino4 = Nodo(actual.x, actual.y + 1, h, actual.g,f)
             vecino4.padre=actual
             lista_vecinos.append(vecino4)
-        
-        # lista_vecinos.sort(key=lambda x: x.f)
 
         return lista_vecinos
         
@@ -61,7 +59,7 @@ class estrella(Nodo):
 
     def __init__(self, mapa, nodo_inicial, nodo_objetivo):
         self.mapa = mapa
-        self.tab = tablero.tablero(21,19)
+        self.tab = tablero.tablero(21,19)#cada busqueda tiene su propio tablero
         self.nodo_inicial = nodo_inicial
         self.nodo_objetivo = nodo_objetivo
 
@@ -72,21 +70,22 @@ class estrella(Nodo):
         # Agrega el nodo inicial a la lista de abiertos
         lista_abiertos.agregar(nodo_inicial)
         while True:
-            check=False
-            if historia != None:
+            check=False#esto es para que solo avance el codigo si no entra a ningun if
+            if len(historia)>0:
                 i=0
                 temporal=lista_abiertos.obtener_nodo_mas_cercano()
                 for casilla in historia:
+
                     if temporal.x == casilla[0] and temporal.y == casilla[1] and temporal.g == casilla[2]:
-                        lista_abiertos.remover(temporal)
                         #esto permite que se quede quieto el agente por un turno
+                        lista_abiertos.remover(temporal)#removemos el nodo y lo actualizamos con un g mayor
                         temporal.g+=1
                         temporal.f+=1
-                        padretemp=copy.copy(temporal.padre)
+                        padretemp=copy.copy(temporal.padre)#creamos un padre nuevo para que no se modifique el padre viejo
                         padretemp.g+=1
                         padretemp.f+=1
-                        padretemp.padre=temporal.padre
-                        temporal.padre=padretemp
+                        padretemp.padre=temporal.padre#le asignamos el padre viejo al padre nuevo ya que nos quedamos quietos
+                        temporal.padre=padretemp#le asignamos el padre nuevo al nodo temporal
                         lista_cerrados.agregar(padretemp)
                         lista_abiertos.agregar(temporal)
                         check=True
@@ -96,24 +95,22 @@ class estrella(Nodo):
                         lista_abiertos.remover(temporal)
                         check=True
                     i+=1
-                #el segundo agente no puede pasar por donde termino el primero
                 if temporal.x==historia[0][0] and temporal.y == historia[0][1] and temporal.g >= historia[0][2]:
+                    #un agente no puede pasar por donde terminaron los agentes anteriores
                     lista_abiertos.remover(temporal)
                     check=True
 
             if not check:    
-                nodo_actual = lista_abiertos.obtener_nodo_mas_cercano()
-                lista_abiertos.remover(nodo_actual)
-                lista_cerrados.agregar(nodo_actual)
-                #agrego el 3 para los nodos de la lista cerrada
-                mapa.actualiza_tablero(nodo_actual.x, nodo_actual.y,"g")
-
+                nodo_actual = lista_abiertos.obtener_nodo_mas_cercano()#obtenemos el nodo con menor f
+                lista_abiertos.remover(nodo_actual)#lo removemos de la lista de abiertos
+                lista_cerrados.agregar(nodo_actual)#lo agregamos a la lista cerrada
+                mapa.actualiza_tablero(nodo_actual.x, nodo_actual.y,"g")#actualizamos el tablero con el nodo al que nos movemos
                 # Si el nodo actual es el objetivo, termina el algoritmo
                 if nodo_actual.x == nodo_objetivo.x and nodo_actual.y ==nodo_objetivo.y:
-                    print("\n\n   anashe")
                     print("\nLlegamos a: "+ str(nodo_actual.x)+","+str(nodo_actual.y))
                     print("\n\nTamaño de la lista abierta: ",len(lista_abiertos.nodos))
                     print("Tamaño de la lista cerrada: ",len(set(lista_cerrados.nodos)))
+                    '''
                     print("Lista abierta:")
                     
                     for i, nodo in enumerate(lista_abiertos.nodos):
@@ -128,24 +125,17 @@ class estrella(Nodo):
                             print(f"Posición {i+1}: X: {nodo.x}, Y: {nodo.y}, G: {nodo.g}, F: {nodo.f}, H: {nodo.h}, Padre: {nodo.padre.x},{nodo.padre.y}")
                         except Exception:
                             print(f"Posición {i+1}: X: {nodo.x}, Y: {nodo.y}, G: {nodo.g}, F: {nodo.f}, H: {nodo.h}, Padre: None")
-                                
-                    # print("Lista abierta: ",lista_abiertos.nodos)
-                    # print("separaion")
-                    # print("Lista cerrada: ",lista_cerrados.nodos)
+                    '''
                     return lista_cerrados.nodos
-                    #return nodo_actual.padre
                 # Expande el nodo actual
                 for nodo_vecino in self.encuentra_vecinos(self.tab, nodo_objetivo, nodo_actual):
-                    # Calcula la distancia estimada hasta el nodo objetivo
-                    costo_estimado = nodo_actual.g + 1 #nodo_vecino.g
-                    nodo_vecino.g = costo_estimado
-                    # Si el nodo vecino no está en la lista de cerrados
-                    #print(lista_abiertos.contiene(nodo_vecino))
+                    costo_estimado = nodo_actual.g + 1 #el costo de ir a un vecino es 1
+                    nodo_vecino.g = costo_estimado #actualizamos el costo del nodo vecino
+                    # Si el nodo vecino no está en la lista de cerrados, lo agrega a la lista de abiertos
                     if not lista_abiertos.contiene(nodo_vecino) and not lista_cerrados.contiene(nodo_vecino):
-                        # Agrega el nodo vecino a la lista de abiertos
                         lista_abiertos.agregar(nodo_vecino)
-                        self.mapa.actualiza_tablero(nodo_vecino.x, nodo_vecino.y, "r")
-                        # Actualiza la distancia estimada del nodo vecino
+                        self.mapa.actualiza_tablero(nodo_vecino.x, nodo_vecino.y, "r")#actualizamos el tablero con el nodo vecino de color rojo
+
                     
                     
                     
